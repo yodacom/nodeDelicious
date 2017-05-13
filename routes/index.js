9f9b12cc8c065bd7cdbf13b52ab81219a82879b3
Jeremy Black <jb@yodacom.com>
2017-05-13 10:33:09 -0600

    updated variable env

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+	const wes = { name: "wes", age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+	// res.json(req.query);
+  res.render("hello", {
+    name:"wes",
+    dog: req.query.dog
+  });
+});
+
+
+router.get("/reverse/:name", (req, res) => {
+	const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
